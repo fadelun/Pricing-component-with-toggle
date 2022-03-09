38ddde35f7454aacfc978a08145c62bd370f0baa
MuhammadFadll <fadeljourney@gmail.com>
2022-03-09 14:13:03 +0700

    update to styling the cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      monst: ["Montserrat", "sans-serif"],
+    },
+
     extend: {
       colors: {
         "blue-primary": "hsl(236, 72%, 79%)",
         "violet-primary": "hsl(237, 63%, 64%)",
+        "white-ghost": "hsl(240, 78%, 98%)",
       },
     },
   },
