9be86ad273c08c205919e922704073b3ea189d45
MuhammadFadll <fadeljourney@gmail.com>
2022-03-09 13:24:38 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "blue-primary": "hsl(236, 72%, 79%)",
+        "violet-primary": "hsl(237, 63%, 64%)",
+      },
+    },
+  },
+  plugins: [],
+};
