0b702dc1816f0299e3be664d9c8e5c461c5db4a7
MuhammadFadll <fadeljourney@gmail.com>
2022-03-13 09:43:01 +0700

    desktop style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
+    screens: {
+      sm: "375px",
+      lg: "1024px",
+    },
+
     fontFamily: {
       monst: ["Montserrat", "sans-serif"],
     },
 
     extend: {
+      backgroundImage: {
+        "background-top": "url('../images/bg-top.svg')",
+      },
       colors: {
         "blue-primary": "hsl(236, 72%, 79%)",
         "violet-primary": "hsl(237, 63%, 64%)",
